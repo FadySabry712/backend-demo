e9346b1dcfd5c788bd05c48ce6dda8284ae2f7b0
FadySabry712 <110053540+FadySabry712@users.noreply.github.com>
2023-09-29 04:25:41 +0000

    Pending changes exported from your codespace

--- medusa-config.js
+++ medusa-config.js
@@ -54,12 +54,16 @@ const plugins = [
     },
   },
   {
-    resolve: `medusa-file-minio`,
+    resolve: `medusa-file-s3`,
     options: {
-        endpoint: process.env.MINIO_ENDPOINT,
-        bucket: process.env.MINIO_BUCKET,
-        access_key_id: process.env.MINIO_ACCESS_KEY,
-        secret_access_key: process.env.MINIO_SECRET_KEY,
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        cache_control: process.env.S3_CACHE_CONTROL,
+        download_file_duration:
+          process.env.S3_DOWNLOAD_FILE_DURATION,
     },
   },
 
